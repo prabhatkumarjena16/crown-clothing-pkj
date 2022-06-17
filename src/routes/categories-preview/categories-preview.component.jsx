df8928b71bbcfe1ca984d02ed869f238222e2471
prabhatkumarjena16 <prabhatkumarjena16@gmail.com>
2022-06-18 00:33:52 +0530

    category, shop, categories route configuration

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+
+import { CategoriesContext } from "../../contexts/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+function CategoriesPreview() {
+  const { categoriesMap } = useContext(CategoriesContext);
+  console.log("categoriesMap", categoriesMap);
+  return (
+    <>
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </>
+  );
+}
+
+export default CategoriesPreview;
